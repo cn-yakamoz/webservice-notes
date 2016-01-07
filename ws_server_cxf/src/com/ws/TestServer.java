fb4f871177d83ef496d1317be8cdb5340e846ef1
cn-yakamoz <cn_yakamoz@sina.com>
2016-01-07 23:43:05 +0800

    增加发布运行条件说明

--- ws_server_cxf/src/com/ws/TestServer.java
+++ ws_server_cxf/src/com/ws/TestServer.java
@@ -4,6 +4,7 @@ import javax.xml.ws.Endpoint;
 
 public class TestServer {
 	public static void main(String[] args) {
+		//必须要导入cxf和jetty的相关包才能运行
 		StudentImpl s=new StudentImpl();
 		String url="http://127.0.0.1:8889/studentws";
 		Endpoint.publish(url, s);
